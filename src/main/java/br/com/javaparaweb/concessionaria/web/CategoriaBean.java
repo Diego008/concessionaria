9f34b6b3bd3035bbf3168b3e8acd09e914bbe332
Diego008 <diegoaugusto008@gmail.com>
2017-07-22 20:33:12 -0300

    criação das classes Java

---
+++ src/main/java/br/com/javaparaweb/concessionaria/web/CategoriaBean.java
@@ -0,0 +1,19 @@
+package br.com.javaparaweb.concessionaria.web;
+
+public class CategoriaBean {
+	private int categoriaId;
+	private String nome;
+	public int getCategoriaId() {
+		return categoriaId;
+	}
+	public void setCategoriaId(int categoriaId) {
+		this.categoriaId = categoriaId;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+}
